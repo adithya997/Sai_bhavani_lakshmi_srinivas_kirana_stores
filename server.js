3ae37a9ee2e3c4ad337395acbb6ce1e92721ada7
adithya997 <adithya.gattu10@gmail.com>
2026-05-26 16:34:51 +0530

    Sucess last

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ let isWhatsappReady = false;
 const getPuppeteerConfig = () => {
     return {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
